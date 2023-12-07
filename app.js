99eaeb8b338abe84068bf07d4008fe1b7a4ced1e
chaleerat <you@example.com>
2023-12-07 20:01:32 +0700

    update edit project on 07/12/2023

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const bcrypt = require('bcrypt')//ไว้เข้ารหัส password
 const saltRounds = 10 //ค่าตัวเลขที่ gen password
 var jwt = require('jsonwebtoken') //
 const secret = 'Fullstack-login-2023'
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function(req,file,cb){
+    return cb(null,"./public/Images")
+},
+filename: function (req,file,cb){
+  return cb(null,`${Date.now()}_${file.originalname}`)
+
+}
+
+})
+const upload = multer({storage})
+
 
 // get the client
 const mysql = require('mysql2');
@@ -39,6 +52,50 @@ app.post('/register',jsonParser, function (req, res, next) {
     // Store hash in your password DB.
 });
   
+})
+
+app.post('/insertimg', upload.single('file'), (req, res)=>{
+  
+ 
+  console.log(req);
+  connection.query(
+    'INSERT INTO banner(banner_name,banner_shop_id) VALUES (?,?)',
+    [req.file.filename,req.body.selectedOption],
+    function(err, results, fields) {
+      console.log(results); // results contains rows returned by server
+      console.log(fields); // fields contains extra meta data about results, if available
+      if(err){
+        res.json({status:'error',message:err})
+        return
+
+      }else{
+        res.json({status:'insert success'})
+      }
+      
+    }
+  );
+ 
+
+})
+
+app.post('/createshop',jsonParser, function (req, res, next) {
+ 
+    connection.query(
+      'INSERT INTO shop(shop_name,address,shop_lat,shop_lng) VALUES (?,?,?,?)',
+      [req.body.shopname,req.body.shopaddress,req.body.shoplat,req.body.shoplng],
+      function(err, results, fields) {
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+        if(err){
+          res.json({status:'error',message:err})
+          return
+  
+        }else{
+          res.json({status:'insert success'})
+        }
+        
+      }
+    );
 })
 
 app.post('/login',jsonParser, function (req, res, next) { 
@@ -79,6 +136,40 @@ app.get('/user', function(req, res) {
       }  
   );
 })
+app.get('/selectshop', function(req, res) { 
+  connection.query(
+    'SELECT shop_id,shop_name FROM shop WHERE NOT EXISTS (SELECT 1 FROM banner WHERE banner.banner_shop_id = shop.shop_id )' ,
+    function(err, results, fields) {
+      console.log(results);
+        if(err){res.json({status:'error',message:err}); return }
+        if(results.length === 0){res.json({message : 'data not found'}); return }
+        res.send(results);
+      }  
+  );
+})
+
+app.get('/banner', function(req, res) { 
+  connection.query(
+    'SELECT * FROM `banner`' ,
+    function(err, results, fields) {
+      console.log(results);
+        if(err){res.json({status:'error',message:err}); return }
+        if(results.length === 0){res.json({message : 'data not found'}); return }
+        res.send(results);
+      }  
+  );
+})
+app.get('/shopalldetail', function(req, res) { 
+  connection.query(
+    'SELECT * FROM shop INNER JOIN banner ON shop.shop_id = banner.banner_shop_id' ,
+    function(err, results, fields) {
+      console.log(results);
+        if(err){res.json({status:'error',message:err}); return }
+        if(results.length === 0){res.json({message : 'data not found'}); return }
+        res.send(results);
+      }  
+  );
+})
 app.get('/shop', function(req, res) { 
   connection.query(
     'SELECT * FROM `shop`' ,
@@ -94,6 +185,41 @@ app.get('/getuserbyid/:id', function(req, res) {
   const id = req.params.id;
   connection.query(
     'SELECT * FROM `user` WHERE user_id = ?' ,id,
+    function(err, results, fields) {
+      console.log(results);
+        if(err){res.json({status:'error',message:err}); return }
+        if(results.length === 0){res.json({message : 'data not found'}); return }
+        else{
+          res.json({status:'success',user:results})
+          return
+      }
+      }  
+  );
+})
+
+app.get('/getShopdetailbyid/:id', function(req, res) { 
+  const id = req.params.id;
+  connection.query(
+    'SELECT * FROM shop INNER JOIN banner ON shop.shop_id = banner.banner_shop_id WHERE shop.shop_id =?' ,id,
+   
+    function(err, results, fields) {
+      console.log(results);
+        if(err){res.json({status:'error',message:err}); return }
+        if(results.length === 0){res.json({message : 'data not found'}); return }
+        else{
+          res.json({status:'success',user:results})
+          return
+      }
+      }  
+  );
+})
+
+
+app.get('/getBannerdetailbyid/:id', function(req, res) { 
+  const id = req.params.id;
+  connection.query(
+    'SELECT * FROM banner WHERE banner_id =?' ,id,
+   
     function(err, results, fields) {
       console.log(results);
         if(err){res.json({status:'error',message:err}); return }
@@ -131,6 +257,47 @@ app.delete('/deleteuser',jsonParser,function(req,res) {
  
   connection.query(
     'DELETE FROM user WHERE user_id = ?',[req.body.id],
+    (err,results)=>{
+      console.log(results)
+      if(err) {
+        res.json({status:'error',message:err})
+        return
+        
+      }else{
+        res.json({status:'success',message:'delete success'})
+        return
+
+      }
+    }
+  );
+
+
+})
+app.delete('/deleteshop',jsonParser,function(req,res) {
+ 
+  connection.query(
+    'DELETE FROM shop WHERE shop_id = ?',[req.body.id],
+    (err,results)=>{
+      console.log(results)
+      if(err) {
+        res.json({status:'error',message:err})
+        return
+        
+      }else{
+        res.json({status:'success',message:'delete success'})
+        return
+
+      }
+    }
+  );
+
+
+})
+
+app.delete('/deletebanner',jsonParser,function(req,res) {
+ 
+  connection.query(
+    'DELETE FROM banner WHERE banner_id = ?',[req.body.id],
     (err,results)=>{
       console.log(results)
       if(err) {
@@ -186,6 +353,14 @@ app.post('/user', function(req, res) {
       }  
   );
 })
+app.get('/public/Images/:filename', (req, res) => {
+  var path = require('path');
+  console.log(req.params.filename);
+ 
+  res.sendFile(req.params.filename, 
+    {root: path.join(__dirname,'/public/Images/')});
+    
+  })
 
 
 
